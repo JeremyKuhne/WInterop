c6f6696238d7590e5cdbf51afd44c31aa6d5e8a0
Jeremy Kuhne <jeremy.kuhne@live.com>
2016-05-15 16:55:36 -0700

    Switch to VS15 templating.

--- src/AutoGenerated/AutoGeneratedConstants.cs
+++ src/AutoGenerated/AutoGeneratedConstants.cs
@@ -29,17 +29,17 @@ namespace WInterop
             /// <summary>
             /// Current day of the month 1..31
             /// </summary>
-            public const int CurrentDayOfMonth =  13;
+            public const int CurrentDayOfMonth =  15;
 
             /// <summary>
             /// Current hour from 0..23
             /// </summary>
-            public const int CurrentHour = 22;
+            public const int CurrentHour = 15;
 
             /// <summary>
             /// Current hour from 0..59
             /// </summary>
-            public const int CurrentMinute = 11;
+            public const int CurrentMinute = 59;
         }
 
         public static class Strings
@@ -62,32 +62,32 @@ namespace WInterop
             /// <summary>
             /// Current day of the month 1..31
             /// </summary>
-            public const string CurrentDayOfMonth =  "13";
+            public const string CurrentDayOfMonth =  "15";
 
             /// <summary>
             /// Current two digit day of the month 01..31
             /// </summary>
-            public const string CurrentTwoDigitDayOfMonth =  "13";
+            public const string CurrentTwoDigitDayOfMonth =  "15";
 
             /// <summary>
             /// Current hour from 0..23
             /// </summary>
-            public const string CurrentHour = "22";
+            public const string CurrentHour = "15";
 
             /// <summary>
             /// Current two digit hour from 00..23
             /// </summary>
-            public const string CurrentTwoDigitHour = "22";
+            public const string CurrentTwoDigitHour = "15";
 
             /// <summary>
             /// Current hour from 0..59
             /// </summary>
-            public const string CurrentMinute = "11";
+            public const string CurrentMinute = "59";
 
             /// <summary>
             /// Current two digit hour from 00..59
             /// </summary>
-            public const string CurrentTwoDigitMinute = "11";
+            public const string CurrentTwoDigitMinute = "59";
         }
     }
 }
