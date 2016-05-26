afccd7fda6eb7f5ed2e2ef562e97caca66204c3f
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2016-05-25 17:35:14 -0700

    Move mailslot handle

--- src/WInterop.Desktop/Ipc/DataTypes/SafeMailslotHandle.cs
+++ src/WInterop.Desktop/Ipc/DataTypes/SafeMailslotHandle.cs
@@ -5,7 +5,9 @@
 // Copyright (c) Jeremy W. Kuhne. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace WInterop.Handles.DataTypes
+using WInterop.Handles.DataTypes;
+
+namespace WInterop.Ipc.DataTypes
 {
     /// <summary>
     /// Safe handle for a mailslot
