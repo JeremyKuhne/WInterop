b2594dd162de723f69a478c276af6b6ade9daaac
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2016-09-04 20:51:47 -0700

    Add VT_DATE conversion method.

--- src/Tests/Tests.Common/Utility/ConversionTests.cs
+++ src/Tests/Tests.Common/Utility/ConversionTests.cs
@@ -43,5 +43,19 @@ namespace WInterop.UtilityTests
         {
             Conversion.FileModeToCreationDisposition(mode).Should().Be(expected);
         }
+
+        [Theory
+            InlineData(0.0d, 599264352000000000)     // [12/30/1899 12:00:00 AM]
+            InlineData(1.0d, 599265216000000000)     // [12/31/1899 12:00:00 AM]
+            InlineData(-1.0d, 599263488000000000)    // [12/29/1899 12:00:00 AM]
+            InlineData(-0.5d, 599264784000000000)    // [12/30/1899 12:00:00 PM]
+            InlineData(0.5d, 599264784000000000)     // [12/30/1899 12:00:00 PM]
+            ]
+        public void OleTimeToDateTime(double oleTime, long expectedTicks)
+        {
+            DateTime expected = new DateTime(expectedTicks);
+            DateTime result = Conversion.OleDateToDateTime(oleTime);
+            result.Should().Be(expected);
+        }
     }
 }
--- src/WInterop/Support/Conversion.cs
+++ src/WInterop/Support/Conversion.cs
@@ -7,13 +7,16 @@
 
 using System;
 using System.Runtime.InteropServices.ComTypes;
-using WInterop.FileManagement;
 using WInterop.FileManagement.DataTypes;
 
 namespace WInterop.Support
 {
     public static class Conversion
     {
+        private static DateTime OleBaseDate = new DateTime(year: 1899, month: 12, day: 30);
+        private const double MinOleDate = -657435.0;
+        private const double MaxOleDate = 2958466.0;
+
         public static ulong HighLowToLong(uint high, uint low)
         {
             return ((ulong)high) << 32 | ((ulong)low & 0xFFFFFFFFL);
@@ -36,7 +39,7 @@ namespace WInterop.Support
                 case System.IO.FileAccess.ReadWrite:
                     return DesiredAccess.GENERIC_READ | DesiredAccess.GENERIC_WRITE;
                 default:
-                    return (DesiredAccess)0;
+                    return 0;
             }
         }
 
@@ -70,6 +73,32 @@ namespace WInterop.Support
                 return CreationDisposition.OPEN_ALWAYS;
             else
                 return unchecked((CreationDisposition)fileMode);
+        }
+
+        /// <summary>
+        /// Convert a native OLE Variant VT_DATE to a DateTime.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if the value is outside of the allowable range for an OLE Date (Dates must be between 100AD and 10000AD).</exception>
+        public static DateTime VariantDateToDateTime(double value)
+        {
+            // The base time for OLE is 12:00:00 AM 12/30/1899, with the integer part being number of
+            // days ahead or behind of the base date and the fractional part being the fraction of a
+            // day _ahead_ of the calculated day. (e.g. 0.75 == -0.75). The fractional day is then rounded
+            // to the nearest half second.
+            //
+            // Dates must be between 100AD and 10000AD.
+            //
+            // https://blogs.msdn.microsoft.com/ericlippert/2003/09/16/erics-complete-guide-to-vt_date/
+
+            if (value < MinOleDate || value > MaxOleDate)
+                throw new ArgumentOutOfRangeException(nameof(value));
+
+            long dayOffsetInTicks = (long)value * TimeSpan.TicksPerDay;
+            long fractionalDayTicks = Math.Abs((long)((value - (long)value) * TimeSpan.TicksPerDay));
+
+            // TODO: Technically we need to round to the nearest half second, but .NET doesn't do this
+
+            return new DateTime(OleBaseDate.Ticks + dayOffsetInTicks + fractionalDayTicks);
         }
     }
 }
