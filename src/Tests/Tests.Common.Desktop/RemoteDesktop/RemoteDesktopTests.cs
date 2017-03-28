6ae82cb22952f2a313128453f02c29b0d085f4d3
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2017-03-27 17:28:48 -0700

    Add basic terminal server apis to see current session and console  session ids.

---
+++ src/Tests/Tests.Common.Desktop/RemoteDesktop/RemoteDesktopTests.cs
@@ -0,0 +1,35 @@
+﻿// ------------------------
+//    WInterop Framework
+// ------------------------
+
+// Copyright (c) Jeremy W. Kuhne. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Xunit;
+using FluentAssertions;
+using WInterop.Desktop.RemoteDesktop;
+using WInterop.ProcessAndThreads;
+
+namespace DesktopTests.RemoteDesktop
+{
+    public class RemoteDesktopTests
+    {
+        [Fact]
+        public void GetCurrentSessionId()
+        {
+            // This, of course, won't necessarily be true if remotely connected to a full Terminal Server
+            // or as an administrator to a Windows server via Remote Desktop for Administration.
+            RemoteDesktopMethods.ProcessIdToSessionId(ProcessMethods.GetCurrentProcessId()).Should().Be(1);
+        }
+
+        [Fact]
+        public void GetActiveConsoleId()
+        {
+            // This, of course, won't necessarily be true if remotely connected to a full Terminal Server
+            // or as an administrator to a Windows server via Remote Desktop for Administration.
+            RemoteDesktopMethods.GetActiveConsoleSessionId().Should().Be(
+                RemoteDesktopMethods.ProcessIdToSessionId(ProcessMethods.GetCurrentProcessId()));
+        }
+    }
+}
---
+++ src/WInterop.Desktop/RemoteDesktop/RemoteDesktopMethods.cs
@@ -0,0 +1,57 @@
+﻿// ------------------------
+//    WInterop Framework
+// ------------------------
+
+// Copyright (c) Jeremy W. Kuhne. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Runtime.InteropServices;
+using WInterop.ErrorHandling;
+
+namespace WInterop.Desktop.RemoteDesktop
+{
+    public static class RemoteDesktopMethods
+    {
+        /// <summary>
+        /// Direct P/Invokes aren't recommended. Use the wrappers that do the heavy lifting for you.
+        /// </summary>
+        /// <remarks>
+        /// By keeping the names exactly as they are defined we can reduce string count and make the initial P/Invoke call slightly faster.
+        /// </remarks>
+        public static class Direct
+        {
+            // https://msdn.microsoft.com/en-us/library/aa382990.aspx
+            [DllImport(Libraries.Kernel32, SetLastError = true, ExactSpelling = true)]
+            public static extern bool ProcessIdToSessionId(
+                uint dwProcessId,
+                out uint pSessionId);
+
+            // https://msdn.microsoft.com/en-us/library/aa383835.aspx
+            [DllImport(Libraries.Kernel32, ExactSpelling = true)]
+            public static extern uint WTSGetActiveConsoleSessionId();
+        }
+
+        /// <summary>
+        /// Get the session Id for the given process
+        /// </summary>
+        /// <param name="processId"></param>
+        /// <returns></returns>
+        public static uint ProcessIdToSessionId(uint processId)
+        {
+            if (!Direct.ProcessIdToSessionId(processId, out uint sessionId))
+                throw ErrorHelper.GetIoExceptionForLastError();
+
+            return sessionId;
+        }
+
+        /// <summary>
+        /// Gets the session ID of the physical console. If there is no session attached,
+        /// returns uint.MaxValue.
+        /// </summary>
+        public static uint GetActiveConsoleSessionId()
+        {
+            return Direct.WTSGetActiveConsoleSessionId();
+        }
+    }
+}
