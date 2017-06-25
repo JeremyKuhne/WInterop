9880c9c6e6900a965e44734f8ecac9e26a027e8e
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2017-06-25 15:37:12 -0700

    Add method for offsetting void pointers.

---
+++ src/Tests/Tests.Common/Support/PointersTests.cs
@@ -0,0 +1,32 @@
+﻿// ------------------------
+//    WInterop Framework
+// ------------------------
+
+// Copyright (c) Jeremy W. Kuhne. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Xunit;
+using FluentAssertions;
+using WInterop.Support;
+
+namespace Tests.Support
+{
+    public class PointersTests
+    {
+        [Fact]
+        public unsafe void PointerOffset()
+        {
+            void* v = (void*)1000;
+            v = Pointers.Offset(v, 42);
+            ((int)v).Should().Be(1042);
+        }
+
+        [Fact]
+        public unsafe void PointerOffset_Negative()
+        {
+            void* v = (void*)1000;
+            v = Pointers.Offset(v, -42);
+            ((int)v).Should().Be(958);
+        }
+    }
+}
---
+++ src/WInterop.Types/Support/Pointers.cs
@@ -0,0 +1,20 @@
+﻿// ------------------------
+//    WInterop Framework
+// ------------------------
+
+// Copyright (c) Jeremy W. Kuhne. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Runtime.CompilerServices;
+
+namespace WInterop.Support
+{
+    public static class Pointers
+    {
+        /// <summary>
+        /// Offset the given pointer by the specified number of bytes.
+        /// </summary>
+        [MethodImpl((MethodImplOptions)16)]
+        public static extern unsafe void* Offset<T>(void* pointer, T offset) where T : struct;
+    }
+}
