1352da10a3636b2c316f8b8b3bdec46bf9564192
Jeremy Kuhne <jeremy.kuhne@live.com>
2017-07-06 15:05:36 -0700

    Start on adding pipe methods

---
+++ src/Shared.WinRT/Pipes/PipeMethods.cs
@@ -0,0 +1,21 @@
+﻿// ------------------------
+//    WInterop Framework
+// ------------------------
+
+// Copyright (c) Jeremy W. Kuhne. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Win32.SafeHandles;
+using WInterop.Support;
+
+namespace WInterop.Pipes
+{
+    public static partial class PipeMethods
+    {
+        public unsafe static void CreatePipe(out SafeFileHandle readPipe, out SafeFileHandle writePipe, uint bufferSize = 0)
+        {
+            if (!Imports.CreatePipe(out readPipe, out writePipe, null, bufferSize))
+                throw Errors.GetIoExceptionForLastError();
+        }
+    }
+}
