a47a3e57b38a1f3c171759758ccf6a5faecf9c09
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2017-10-10 18:30:22 -0700

    Break out volume method imports

---
+++ src/WInterop.Desktop/VolumeManagement/VolumeMethods.Imports.cs
@@ -0,0 +1,87 @@
+﻿// ------------------------
+//    WInterop Framework
+// ------------------------
+
+// Copyright (c) Jeremy W. Kuhne. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Runtime.InteropServices;
+using WInterop.VolumeManagement.Types;
+
+namespace WInterop.VolumeManagement
+{
+    public static partial class VolumeMethods
+    {
+        /// <summary>
+        /// Direct usage of Imports isn't recommended. Use the wrappers that do the heavy lifting for you.
+        /// </summary>
+        public static partial class Imports
+        {
+            // https://msdn.microsoft.com/en-us/library/windows/desktop/aa365461.aspx
+            [DllImport(Libraries.Kernel32, CharSet = CharSet.Unicode, SetLastError = true, ExactSpelling = true)]
+            public static extern uint QueryDosDeviceW(
+                string lpDeviceName,
+                SafeHandle lpTargetPath,
+                uint ucchMax);
+
+            // https://msdn.microsoft.com/en-us/library/windows/desktop/aa364972.aspx
+            [DllImport(Libraries.Kernel32, SetLastError = true, ExactSpelling = true)]
+            public static extern uint GetLogicalDrives();
+
+            // https://msdn.microsoft.com/en-us/library/windows/desktop/aa364975.aspx
+            [DllImport(Libraries.Kernel32, CharSet = CharSet.Unicode, SetLastError = true, ExactSpelling = true)]
+            public static extern uint GetLogicalDriveStringsW(
+                uint nBufferLength,
+                SafeHandle lpBuffer);
+
+            // https://msdn.microsoft.com/en-us/library/windows/desktop/aa364996.aspx
+            [DllImport(Libraries.Kernel32, CharSet = CharSet.Unicode, SetLastError = true, ExactSpelling = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool GetVolumePathNameW(
+                string lpszFileName,
+                SafeHandle lpszVolumePathName,
+                uint cchBufferLength);
+
+            // https://msdn.microsoft.com/en-us/library/windows/desktop/aa364998.aspx
+            [DllImport(Libraries.Kernel32, CharSet = CharSet.Unicode, SetLastError = true, ExactSpelling = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool GetVolumePathNamesForVolumeNameW(
+                string lpszVolumeName,
+                SafeHandle lpszVolumePathNames,
+                uint cchBuferLength,
+                ref uint lpcchReturnLength);
+
+            // https://msdn.microsoft.com/en-us/library/windows/desktop/aa364994.aspx
+            [DllImport(Libraries.Kernel32, CharSet = CharSet.Unicode, SetLastError = true, ExactSpelling = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool GetVolumeNameForVolumeMountPointW(
+                string lpszVolumeMountPoint,
+                SafeHandle lpszVolumeName,
+                uint cchBufferLength);
+
+            // https://msdn.microsoft.com/en-us/library/windows/desktop/aa364993.aspx
+            [DllImport(Libraries.Kernel32, CharSet = CharSet.Unicode, SetLastError = true, ExactSpelling = true)]
+            public static extern bool GetVolumeInformationW(
+                string lpRootPathName,
+                SafeHandle lpVolumeNameBuffer,
+                uint nVolumeNameSize,
+                out uint lpVolumeSerialNumber,
+                out uint lpMaximumComponentLength,
+                out FileSystemFeature lpFileSystemFlags,
+                SafeHandle lpFileSystemNameBuffer,
+                uint nFileSystemNameSize);
+
+            // https://msdn.microsoft.com/en-us/library/windows/desktop/aa364939.aspx
+            [DllImport(Libraries.Kernel32, CharSet = CharSet.Unicode, ExactSpelling = true)]
+            public static extern DriveType GetDriveTypeW(
+                string lpRootPathName);
+
+            // https://msdn.microsoft.com/en-us/library/windows/desktop/aa364433.aspx
+            [DllImport(Libraries.Kernel32, SetLastError = true, ExactSpelling = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool FindVolumeClose(
+                IntPtr hFindVolume);
+        }
+    }
+}
--- src/WInterop.Desktop/VolumeManagement/VolumeMethods.cs
+++ src/WInterop.Desktop/VolumeManagement/VolumeMethods.cs
@@ -5,10 +5,7 @@
 // Copyright (c) Jeremy W. Kuhne. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Runtime.InteropServices;
 using WInterop.ErrorHandling.Types;
 using WInterop.Support;
 using WInterop.Support.Buffers;
@@ -19,80 +16,8 @@ namespace WInterop.VolumeManagement
     public static partial class VolumeMethods
     {
         /// <summary>
-        /// Direct usage of Imports isn't recommended. Use the wrappers that do the heavy lifting for you.
-        /// </summary>
-        public static partial class Imports
-        {
-            // https://msdn.microsoft.com/en-us/library/windows/desktop/aa365461.aspx
-            [DllImport(Libraries.Kernel32, CharSet = CharSet.Unicode, SetLastError = true, ExactSpelling = true)]
-            public static extern uint QueryDosDeviceW(
-                string lpDeviceName,
-                SafeHandle lpTargetPath,
-                uint ucchMax);
-
-            // https://msdn.microsoft.com/en-us/library/windows/desktop/aa364972.aspx
-            [DllImport(Libraries.Kernel32, SetLastError = true, ExactSpelling = true)]
-            public static extern uint GetLogicalDrives();
-
-            // https://msdn.microsoft.com/en-us/library/windows/desktop/aa364975.aspx
-            [DllImport(Libraries.Kernel32, CharSet = CharSet.Unicode, SetLastError = true, ExactSpelling = true)]
-            public static extern uint GetLogicalDriveStringsW(
-                uint nBufferLength,
-                SafeHandle lpBuffer);
-
-            // https://msdn.microsoft.com/en-us/library/windows/desktop/aa364996.aspx
-            [DllImport(Libraries.Kernel32, CharSet = CharSet.Unicode, SetLastError = true, ExactSpelling = true)]
-            [return: MarshalAs(UnmanagedType.Bool)]
-            public static extern bool GetVolumePathNameW(
-                string lpszFileName,
-                SafeHandle lpszVolumePathName,
-                uint cchBufferLength);
-
-            // https://msdn.microsoft.com/en-us/library/windows/desktop/aa364998.aspx
-            [DllImport(Libraries.Kernel32, CharSet = CharSet.Unicode, SetLastError = true, ExactSpelling = true)]
-            [return: MarshalAs(UnmanagedType.Bool)]
-            public static extern bool GetVolumePathNamesForVolumeNameW(
-                string lpszVolumeName,
-                SafeHandle lpszVolumePathNames,
-                uint cchBuferLength,
-                ref uint lpcchReturnLength);
-
-            // https://msdn.microsoft.com/en-us/library/windows/desktop/aa364994.aspx
-            [DllImport(Libraries.Kernel32, CharSet = CharSet.Unicode, SetLastError = true, ExactSpelling = true)]
-            [return: MarshalAs(UnmanagedType.Bool)]
-            public static extern bool GetVolumeNameForVolumeMountPointW(
-                string lpszVolumeMountPoint,
-                SafeHandle lpszVolumeName,
-                uint cchBufferLength);
-
-            // https://msdn.microsoft.com/en-us/library/windows/desktop/aa364993.aspx
-            [DllImport(Libraries.Kernel32, CharSet = CharSet.Unicode, SetLastError = true, ExactSpelling = true)]
-            public static extern bool GetVolumeInformationW(
-                string lpRootPathName,
-                SafeHandle lpVolumeNameBuffer,
-                uint nVolumeNameSize,
-                out uint lpVolumeSerialNumber,
-                out uint lpMaximumComponentLength,
-                out FileSystemFeature lpFileSystemFlags,
-                SafeHandle lpFileSystemNameBuffer,
-                uint nFileSystemNameSize);
-
-            // https://msdn.microsoft.com/en-us/library/windows/desktop/aa364939.aspx
-            [DllImport(Libraries.Kernel32, CharSet = CharSet.Unicode, ExactSpelling = true)]
-            public static extern DriveType GetDriveTypeW(
-                string lpRootPathName);
-
-            // https://msdn.microsoft.com/en-us/library/windows/desktop/aa364433.aspx
-            [DllImport(Libraries.Kernel32, SetLastError = true, ExactSpelling = true)]
-            [return: MarshalAs(UnmanagedType.Bool)]
-            public static extern bool FindVolumeClose(
-                IntPtr hFindVolume);
-        }
-
-        /// <summary>
         /// Returns the mapping for the specified DOS device name or the full list of DOS device names if passed null.
         /// </summary>
-        [SuppressMessage("Microsoft.Interoperability", "CA1404:CallGetLastErrorImmediatelyAfterPInvoke")]
         public static IEnumerable<string> QueryDosDevice(string deviceName)
         {
             if (deviceName != null) deviceName = Paths.TrimTrailingSeparators(deviceName);
@@ -131,7 +56,6 @@ namespace WInterop.VolumeManagement
         /// <summary>
         /// Gets a set of strings for the defined logical drives in the system.
         /// </summary>
-        [SuppressMessage("Microsoft.Interoperability", "CA1404:CallGetLastErrorImmediatelyAfterPInvoke")]
         public static IEnumerable<string> GetLogicalDriveStrings()
         {
             return BufferHelper.BufferInvoke((StringBuffer buffer) =>
@@ -155,7 +79,6 @@ namespace WInterop.VolumeManagement
         /// <summary>
         /// Returns the path of the volume mount point for the specified path.
         /// </summary>
-        [SuppressMessage("Microsoft.Interoperability", "CA1404:CallGetLastErrorImmediatelyAfterPInvoke")]
         public static string GetVolumePathName(string path)
         {
             return BufferHelper.BufferInvoke((StringBuffer buffer) =>
@@ -182,7 +105,6 @@ namespace WInterop.VolumeManagement
         /// Returns the list of all paths where the given volume name is mounted.
         /// </summary>
         /// <param name="volumeName">A volume GUID path for the volume (\\?\Volume{xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx}\).</param>
-        [SuppressMessage("Microsoft.Interoperability", "CA1404:CallGetLastErrorImmediatelyAfterPInvoke")]
         public static IEnumerable<string> GetVolumePathNamesForVolumeName(string volumeName)
         {
             return BufferHelper.BufferInvoke((StringBuffer buffer) =>
