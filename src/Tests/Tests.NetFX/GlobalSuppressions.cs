d71886a9002725b13002d148d972ac3a817cb063
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2018-01-19 14:13:55 -0800

    Suppress IDE0039

---
+++ src/Tests/Tests.NetFX/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0039:Use local function", Justification = "Using delegates needed for exception verification")]
+
