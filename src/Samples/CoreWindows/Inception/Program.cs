9ab8308f356179a3b6730f3f4dce512738ab4445
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2018-08-01 21:13:32 -0700

    Break inception class out

--- src/Samples/CoreWindows/Inception/Program.cs
+++ src/Samples/CoreWindows/Inception/Program.cs
@@ -6,11 +6,9 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.Drawing;
-using WInterop.Gdi;
 using WInterop.Windows;
 
-namespace BallBounce
+namespace Inception
 {
     static class Program
     {
@@ -21,38 +19,4 @@ namespace BallBounce
         }
     }
 
-    public class Inception : WindowClass
-    {
-        Rectangle _client;
-        Rectangle _screen;
-
-        public Inception() : base(backgroundBrush: BrushHandle.NoBrush) { }
-
-        protected override LRESULT WindowProcedure(WindowHandle window, WindowMessage message, WPARAM wParam, LPARAM lParam)
-        {
-            switch (message)
-            {
-                case WindowMessage.Create:
-                    _screen = new Rectangle(0, 0, Windows.GetSystemMetrics(SystemMetric.ScreenWidth), Windows.GetSystemMetrics(SystemMetric.ScreenHeight));
-                    return 0;
-                case WindowMessage.Size:
-                    _client = new Rectangle(default, new Message.Size(wParam, lParam).NewSize);
-                    return 0;
-                case WindowMessage.Move:
-                    window.Invalidate();
-                    return 0;
-                case WindowMessage.Paint:
-                    using (DeviceContext clientDC = window.BeginPaint())
-                    using (DeviceContext screenDC = Gdi.GetDeviceContext())
-                    using (DeviceContext memoryDC = clientDC.CreateCompatibleDeviceContext())
-                    {
-                        clientDC.SetStretchBlitMode(StretchMode.HalfTone);
-                        screenDC.StretchBlit(clientDC, _screen, _client, RasterOperation.Common.SourceCopy);
-                    }
-                    return 0;
-            }
-
-            return base.WindowProcedure(window, message, wParam, lParam);
-        }
-    }
 }
