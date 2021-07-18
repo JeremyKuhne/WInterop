373788a8dddac1104ef44af382e95a2ef7ec2db9
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2021-07-18 12:09:43 -0700

    Add PrintClient message helper

---
+++ src/WInterop.Desktop/Windows/Messages/Message.PrintClient.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Jeremy W. Kuhne. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using WInterop.Gdi;
+using WInterop.Gdi.Native;
+
+namespace WInterop.Windows
+{
+    public static partial class Message
+    {
+        public readonly ref struct PrintClient
+        {
+            public HDC HDC { get; }
+
+            public DeviceContext DeviceContext => new(HDC);
+
+            public PrintClient(WParam wParam)
+            {
+                HDC = new(wParam);
+            }
+        }
+    }
+}
